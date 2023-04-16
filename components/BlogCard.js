4a18411f90ac9699b72d0606066b7d90d8b6af51
inwookim <dlsdn166@gmail.com>
2023-04-16 18:16:04 +0900

    feat : 카드 영역 추가

--- components/BlogCard.js
+++ components/BlogCard.js
@@ -1,14 +1,43 @@
 import Link from 'next/link';
 import styles from '../src/styles/BlogCard.module.css';
 
-export function BlogPost({title,author,coverPhoto,datePublished,slug}){
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { Avatar } from '@mui/material';
+
+export default function BlogPost({title,author,coverPhoto,dataPublished,slug}){
     return (
-        <div className="{styles.card}">
+        <div className={styles.card}>
             <Link href={"/posts/"+slug}>
-                <div className='{styles.imgContainer}'>
-                    <img scr="coverPhoto.url" alt ="" />
-                </div>
+                <CardMedia 
+                component="img"
+                height="20%"
+                width="150px"
+                image={coverPhoto.url}
+                alt=""
+                />
             </Link>
+            <CardContent>
+                <Typography gutterBottom variant='h4' component="div">
+                    {title}
+                </Typography>
+                <div className={styles.cardContentBottom}>
+                    
+                    <div className={styles.cardContentBottomArea}>
+                        <div className={styles.cardContentBottomLeft}>
+                            <Avatar alt="Cindy Baker" src={author.avatar.url}/> 
+                            <div className={styles.cardContentBottomAuthorName}>{author.name}</div>
+                        </div>
+
+                        <div className={styles.cardContentBottomRight}>
+                            {dataPublished}
+                        </div>
+                    </div>
+                </div>
+            </CardContent>            
         </div>
-    );
+    )
 }
